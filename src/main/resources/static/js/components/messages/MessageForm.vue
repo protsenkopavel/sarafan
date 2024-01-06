7a0b9852fd8722a6f2c15924df8ef92b4d16a5f3
Pavel Protsenko <pavelprotsenko@outlook.com>
2024-01-07 00:12:08 +0300

    Add vuetify

--- src/main/resources/static/js/components/messages/MessageForm.vue
+++ src/main/resources/static/js/components/messages/MessageForm.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>
-    <input type="text" placeholder="Write something" v-model="text"/>
-    <input type="button" value="Save" v-on:click="save"/>
-  </div>
+  <v-layout row>
+    <v-text-field
+        label="New message"
+        placeholder="Write something"
+        v-model="text"
+    />
+    <v-btn @click="save">
+      Save
+    </v-btn>
+  </v-layout>
 </template>
 
 <script>
-import {sendMessage} from "util/ws";
+import { sendMessage } from 'util/ws'
 
 export default {
   props: ['messages', 'messageAttr'],
@@ -17,36 +23,35 @@ export default {
     }
   },
   watch: {
-    messageAttr: function (newVal, oldVal) {
+    messageAttr(newVal, oldVal) {
       this.text = newVal.text
       this.id = newVal.id
     }
   },
-
   methods: {
     save() {
-      sendMessage({id:this.id, text:this.text})
+      sendMessage({id: this.id, text: this.text})
       this.text = ''
       this.id = ''
 
-      /*const message = {text: this.text}
+      /*const message = { text: this.text }
 
       if (this.id) {
-        this.$resource('/message{/id}').update({id: this.id}, message).then(result =>
-            result.json().then(data => {
-              const index = getIndex(this.messages, data.id);
-              this.messages.splice(index, 1, data)
-              this.text = ''
-              this.id = ''
-            }))
+          this.$resource('/message{/id}').update({id: this.id}, message).then(result =>
+              result.json().then(data => {
+                  const index = getIndex(this.messages, data.id)
+                  this.messages.splice(index, 1, data)
+                  this.text = ''
+                  this.id = ''
+              })
+          )
       } else {
-        this.$resource('/message{/id}').save({}, message).then(result =>
-            result.json().then(data => {
-              this.messages.push(data)
-              this.text = ''
-            })
-        )
-
+          this.$resource('/message{/id}').save({}, message).then(result =>
+              result.json().then(data => {
+                  this.messages.push(data)
+                  this.text = ''
+              })
+          )
       }*/
     }
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { VueLoaderPlugin } = require('vue-loader')
+const {VueLoaderPlugin} = require('vue-loader')
 module.exports = {
     mode: 'development',
     devtool: 'source-map',
@@ -29,6 +29,13 @@ module.exports = {
             {
                 test: /\.vue$/,
                 loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
             }
         ]
     },
