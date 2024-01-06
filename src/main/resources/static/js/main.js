6b4fbc859dca9ca3a79525c975302b83b8961576
Pavel Protsenko <pavelprotsenko@outlook.com>
2024-01-07 01:43:27 +0300

    Add vuex

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,17 +1,20 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import '@babel/polyfill'
 import 'api/resource'
 import App from 'pages/App.vue'
-import {connect} from "./util/ws";
+import store from 'store/store'
+import { connect } from './util/ws'
 import 'vuetify/dist/vuetify.min.css'
 
 if (frontendData.profile) {
     connect()
 }
 
-Vue.useAttrs(Vuetify)
+Vue.use(Vuetify)
 
 new Vue({
     el: '#app',
+    store,
     render: a => a(App)
 })
